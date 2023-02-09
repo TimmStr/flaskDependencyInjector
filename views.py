a46ec04453e7686653d65e08f0f954a6aa7aa785
achilleus <timm.straub@gmx.de>
2023-02-09 13:36:38 +0100

    Yaml und Views Refactoring

--- views.py
+++ views.py
@@ -5,9 +5,14 @@ from containers import Container
 
 
 @inject
-def index(search_service: SearchService = Provide[Container.search_service]):
-    query = request.args.get("query", "Dependency Injector")
-    limit = request.args.get("limit", 10, int)
+def index(
+        search_service: SearchService = Provide[Container.search_service],
+        default_query: str = Provide[Container.config.default.query],
+        default_limit: int = Provide[Container.config.default.limit.as_int()],
+):
+    query = request.args.get("query", default_query)
+    limit = request.args.get("limit", default_limit, int)
+
     repositories = search_service.search_repositories(query, limit)
 
     return render_template(
